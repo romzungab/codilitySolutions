68aa8cf6d185fc14555dfcba43fda9f8e3cba3c5
Roms <romelyn.ungab@ipfx.com>
2019-04-03 11:30:52 +1300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Convert.ToString(2, 2).ToCharArray();
+
+            var length = s.Length;
+            var bg = 0;
+            var max = 0;
+            for (var i = 0; i < length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    bg++;
+                }
+                else
+                {
+                    if (max < bg)
+                    {
+                        max = bg;
+                        
+                    }
+                    bg = 0;
+                }
+
+                }
+
+            Console.Out.WriteLine(max);
+        }
+
+    }
+}
