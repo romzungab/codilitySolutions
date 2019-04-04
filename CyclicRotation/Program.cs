362783fee681837d7d0589f1ceec873fb60f3c9b
Roms <romelyn.ungab@ipfx.com>
2019-04-04 14:09:54 +1300

    Cyclic Rotation and OddOccurence

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclicRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = 3;
+            int[] a = { 3, 8, 9, 7, 6 };
+
+            if (k == 0 || a.Length == 0)
+            {
+                foreach (var i in a)
+                    Console.Out.Write(i);
+            }
+            else
+            {
+                var len = a.Length;
+                int[] rot = new int[len];
+
+                for (var i = 0; i < len; i++)
+                {
+                    if (i < (k % len))
+                        rot[i] = a[(len - (k % len)) + i];
+                    else
+                        rot[i] = a[Math.Abs((k % len) - i)];
+
+                }
+
+                foreach (var i in rot)
+                    Console.Out.Write(i);
+                Console.Out.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ OddOccurences/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace OddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 9, 3, 9, 3, 9, 7, 9, 7, 9,9,9,9,7,7,9,244,9, 244, 523232 , 2,2};
+            Console.Out.WriteLine(a.Aggregate((x, y) => x ^ y)); //using aggregate and XOR operator
+        }
+    }
+}
